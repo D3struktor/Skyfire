6580f20dcd27d5dfa690bc542d303a96fd615d57
D3struktor <d3struktor69@gmail.com>
2024-05-31 17:57:12 +0200

    Multiplayer - 1st step
    Pierwszy krok w strone solidnego multiplayera

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,55 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+using System.IO;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
+public class PlayerManager : MonoBehaviour
 {
-    private Vector3 realPosition = Vector3.zero;
-    private Quaternion realRotation = Quaternion.identity;
+    PhotonView PV;
 
-    void Start()
-    {
-        if (photonView.IsMine)
-        {
-            // To jest nasz lokalny gracz, możesz dodać tutaj kod do sterowania
-            GetComponent<MeshRenderer>().material.color = Color.blue; // Przykład: zmiana koloru lokalnego gracza
-        }
-        else
-        {
-            // To jest zdalny gracz
-            GetComponent<MeshRenderer>().material.color = Color.red; // Przykład: zmiana koloru zdalnego gracza
-        }
-    }
+    void Awake()
+	{
+		PV = GetComponent<PhotonView>();
+	}
 
-    void Update()
-    {
-        if (!photonView.IsMine)
-        {
-            // Interpolacja pozycji i rotacji zdalnych graczy
-            transform.position = Vector3.Lerp(transform.position, realPosition, Time.deltaTime * 10);
-            transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, Time.deltaTime * 10);
-        }
-        else
-        {
-            // Sterowanie lokalnym graczem (przykład)
-            float move = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
-            float strafe = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
-            transform.Translate(strafe, 0, move);
-        }
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            // Dane, które wysyłamy do innych graczy
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
-        }
-        else
-        {
-            // Dane, które odbieramy od innych graczy
-            realPosition = (Vector3)stream.ReceiveNext();
-            realRotation = (Quaternion)stream.ReceiveNext();
-        }
+	void Start()
+	{
+		if(PV.IsMine)
+		{
+			CreateController();
+		}
+	}
+    
+	void CreateController()
+	{
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
     }
 }
